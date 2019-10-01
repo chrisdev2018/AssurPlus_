6de4357d48311ab2826a4a667b4f4f41612c33a2
Christian FOMEKONG <christian.fomekong@gmail.com>
2019-10-01 19:42:39 +0100

    [ADD] First commit

---
+++ assurplus/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "AssurPlus",
+
+    'summary': """Gestion des assurances automobiles""",
+
+    'description': """
+        Ce module est conçu pour la gestion automatique de la
+        procdure de traitement des opérations dans un bureau d'assurance.
+        Permet de Collecter les informations d'un assuré ainsi que celles de son véhicule
+        de les sauvegarder, ensuite de les restituer au besoin.
+    """,
+
+    'author': "Christian FOMEKONG",
+    'website': "https://github.com/chrisdev2018",
+
+    'category': 'Dev',
+    'version': '0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/menus_views.xml',
+        'views/first_child_views.xml'
+        
+    ],
+    'installable': True,
+    'application': True,
+    
+}
