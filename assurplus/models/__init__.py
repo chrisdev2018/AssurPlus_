9be447c05f8d4f44fb45a1161459cea47f91323f
Christian FOMEKONG <christian.fomekong@gmail.com>
2019-10-03 06:57:17 +0100

    [ADD] Ajoute le model prime

--- assurplus/models/__init__.py
+++ assurplus/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import assure
+from . import prime
---
+++ assurplus/models/prime.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+class Prime(models.Model):
+    _name = "assurplus.prime"
+
+    name = fields.Char(
+        string="Libellé",
+        required=True
+    )
+
+    valeur = fields.Float(
+        string="Valeur"
+    )
+
+    garantie = fields.Char(
+        string="Limite de garantie"
+    )
+
+    reduction = fields.Float(
+        string="Réduction(%)"
+    )
+
+    majoration = fields.Float(
+        string="Majoration(%)"
+    )
+
+    comptant = fields.Float(
+        string="Comptant",
+        compute="calcul_comptant",
+        store=True
+    )
+
+    @api.multi
+    def calcul_comptant(self):
+        return True
+
