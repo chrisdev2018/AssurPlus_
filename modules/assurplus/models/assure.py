19f8494377a2fbc09cf7ca338705be726d80f695
Christian FOMEKONG <christian.fomekong@gmail.com>
2019-12-15 13:18:23 +0100

    [ADD] Ajoute le smartbutton pour afficher les véhicules dont l'assuré est conducteur

--- modules/assurplus/models/assure.py
+++ modules/assurplus/models/assure.py
@@ -29,6 +29,11 @@ class Assure(models.Model):
         string='Contrats',
         compute='get_count_contrats')
     
+    count_cars = fields.Integer(
+        compute='get_count_cars', 
+        string='Véhicules')
+    
+    
     @api.multi
     def open_conditions_parti(self):
         return {
@@ -44,4 +49,19 @@ class Assure(models.Model):
     def get_count_contrats(self):
         count = self.env['assurplus.conditions_parti'].search_count([('assure_id', '=', self.id)])
         self.count_contrats = count 
+        
+    def get_count_cars(self):
+        count = self.env['assurplus.vehicule'].search_count([('conducteur', '=', self.id)])
+        self.count_cars = count
+        
+    def open_vehicules(self):
+        return {
+            'name': ('Véhicules'),
+            'domain': [('conducteur', '=', self.id)],
+            'view_type': 'form',
+            'view_id': False,
+            'res_model': 'assurplus.vehicule',
+            'view_mode': 'tree,form',
+            'type': 'ir.actions.act_window',
+        }    
     
